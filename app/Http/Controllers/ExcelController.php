9b5214a0a0e62baff60c4df7874488284d95c4e1
diesel <diesel.kapasule@speedandfunction.com>
2023-09-18 13:01:10 +0200

    Removing unused import

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Rules\MaxFileSize;
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
