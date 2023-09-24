bef5ba70300c2c5c2895febf94e7dcf07ad7616b
diesel <diesel.kapasule@speedandfunction.com>
2023-09-24 23:50:09 +0200

    Updating home route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [ExcelController::class, 'showForm']);
 Route::get('/import', [ExcelController::class, 'showForm']);
 Route::post('/import', [ExcelController::class, 'import']);
